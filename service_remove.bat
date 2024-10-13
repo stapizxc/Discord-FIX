aa1324485c2a5681bea0fb73933f79fbfc8a7bcf
stapizxc <pro229bs@gmail.com>
2024-10-13 15:51:32 +0300

    Add files via upload

--- service_remove.bat
+++ service_remove.bat
@@ -2,24 +2,19 @@
 chcp 65001 >nul
 :: 65001 - UTF-8
 
-:: Path check
-set scriptPath=%~dp0
-set "path_no_spaces=%scriptPath: =%"
-if not "%scriptPath%"=="%path_no_spaces%" (
-    echo Путь содержит пробелы. 
-    echo Пожалуйста, переместите скрипт в директорию без пробелов.
-    pause
-    exit /b
-)
-
 :: Admin rights check
-echo Данный файл должен быть запущен с правами администратора (ПКМ -> Запустить от имени администратора).
+echo Данный файл должен быть запущен с правами администратора (ПКМ - Запустить от имени администратора).
 echo Нажмите любую клавишу, чтобы продолжить удаление и остановку сервиса.
 pause
 
 set SRVCNAME=zapret
 
-net stop "%SRVCNAME%"
-sc delete "%SRVCNAME%"
+net stop %SRVCNAME%
+sc delete %SRVCNAME%
+
+net stop "WinDivert"
+sc delete "WinDivert"
+net stop "WinDivert14"
+sc delete "WinDivert14"
 
 pause
